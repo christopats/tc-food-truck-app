c03027362136adda62744e09d67670a17684d6c9
christopats <christopattison@gmail.com>
2025-01-17 15:32:20 +0000

    remove all dependencies and start again hello world in app/index.tsx running in npx expo start

--- food-truck-mobile/app/_layout.tsx
+++ food-truck-mobile/app/_layout.tsx
@@ -1,10 +1,5 @@
-import React from "react";
 import { Stack } from "expo-router";
 
-const Layout = () => (
-  <Stack>
-    <Stack.Screen name="index" options={{ title: "Home" }} />
-  </Stack>
-);
-
-export default Layout;
+export default function RootLayout() {
+  return <Stack />;
+}
--- food-truck-mobile/app/index.tsx
+++ food-truck-mobile/app/index.tsx
@@ -1,45 +1,18 @@
-import { useRouter } from "expo-router";
-import React from "react";
-import { View, StyleSheet, Button } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, Text, View } from "react-native";
 
-const HomeScreen = () => {
-  const router = useRouter();
-
-  const navigateToAuth = () => {
-    router.push("/(auth)/login");
-  };
-
+export default function Index() {
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title="Marker Title"
-          description="Marker Description"
-        />
-      </MapView>
-      <Button title="Go to Auth Page" onPress={navigateToAuth} />
+      <Text>Hello, World!</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
     flex: 1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-export default HomeScreen;
